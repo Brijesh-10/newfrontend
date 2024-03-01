c67372f4f83f29a6fef61bfb9f0667285c55fe93
Brijesh <brijes1820h@gmail.com>
2024-03-01 21:55:16 +0530

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import React from 'react'
+import { Link,useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai';
-import { Navbar, TextInput } from 'flowbite-react';
-
+import { FaMoon} from 'react-icons/fa';
 export default function Header() {
+    const path=useLocation().pathname;
+  return (
+    <Navbar className='border-b-2 bg-gray-800'>
+         <div className="flex items-center">
+                <div className='mr-4 lg:mr-0'>
+                    <TextInput
+                        type='text'
+                        placeholder='Search...'
+                        rightIcon={AiOutlineSearch}
+                        className=' lg:inline w-full lg:w-64'
+                    />
+                </div>
+                {/* <Button className='w-14 h-10' color='gray'>
+                    <AiOutlineSearch/>
+                </Button> */}
+            </div>
+        <div className='flex gap-2 md:order-2'>
+            <Navbar.Toggle/>
+        </div>
+        <Navbar.Collapse className=" gap-4 md:gap-10">
+    <Navbar.Link className='text-cyan-600'><Link to='#'>Categories</Link></Navbar.Link>
+    <Navbar.Link className='text-cyan-600'><Link to='#'>Website Builders</Link></Navbar.Link>
+    <Navbar.Link className='text-cyan-600'><Link to='#'>Today's deals</Link></Navbar.Link>
+    <Navbar.Link className='text-cyan-600'><Link to='#'>Projects</Link></Navbar.Link>
+</Navbar.Collapse>
 
-    return (
-        <Navbar className='border-b-2 bg-gray-800 text-white'>
-            <Navbar.Collapse>
-                <Navbar.Link><Link to='#' className="text-white">Categories</Link></Navbar.Link>
-                <Navbar.Link><Link to='#' className="text-white">Website Builders</Link></Navbar.Link>
-                <Navbar.Link><Link to='#' className="text-white">Today's deals</Link></Navbar.Link>
-            </Navbar.Collapse>
-            <div className='flex gap-2 md:order-2'>
-                <Navbar.Toggle />
-            </div>
-            <div className='flex items-center ml-auto'>
-                <TextInput
-                    type='text'
-                    rightIcon={AiOutlineSearch}
-                    className='w-48 md:w-40 sm:w-35 h-10 text-black px-2 py-1 rounded' 
-                />
-            </div>
-        </Navbar>
-    );
+    </Navbar>
+  )
 }
